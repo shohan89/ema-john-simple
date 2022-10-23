7d23ac2f61eead456447c48fd284c045a57cf22e
shoh4n <maffin.killer007@gmail.com>
2022-10-23 16:10:04 +0600

    Cart system added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
+  console.log( cart );
+  let total = 0;
+  let shipping = 0;
+
+  for ( const product of cart ) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  const tax = parseFloat(total * 0.1)
+  const grandTotal = total + shipping + tax;
+
   return (
-    <div>
+    <div className='cart'>
       <h2>Order summery</h2>
         <p>Selected Items: { cart.length }</p>
+        <p>Total Price: ${ total }</p>
+        <p>Total Shipping: ${ shipping }</p>
+        <p>Tax: ${ tax.toFixed( 2 ) }</p>
+        <h5>Grand Total: ${ grandTotal }</h5>
     </div>
   );
 };
