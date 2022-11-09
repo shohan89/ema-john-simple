aa042306cb1805705e75e28d1c13f76ef8573a1a
shoh4n <maffin.killer007@gmail.com>
2022-11-09 12:54:50 +0600

    React Router Added to this shop!!

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Header.css'
 import logo from "../../images/Logo.svg"
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <nav className='header'>
       <img src={ logo } alt="" />
       <div>
-        <a href="/shop" target="_blank" rel="noopener noreferrer">Shop</a>
-        <a href="orders" target="_blank" rel="noopener noreferrer">Orders</a>
-        <a href="inventory" target="_blank" rel="noopener noreferrer">Inventory</a>
-        <a href="about" target="_blank" rel="noopener noreferrer">About</a>
+        <Link to="/">Shop</Link>
+        <Link to="orders">Orders</Link>
+        <Link to="inventory">Inventory</Link>
+        <Link to="about">About</Link>
       </div>
     </nav>
   );
